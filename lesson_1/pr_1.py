fcc3ccfdc9de8c84e1d7a9935c1ad4e5d078eebc
Kirill Sheludchenko <kirill.shelydchenko@gmail.com>
2023-09-06 16:57:19 +0300

    add pr1

---
+++ lesson_1/pr_1.py
@@ -0,0 +1,18 @@
+#   створюємо змінні з ім'ям та фамілією куди користувач буде сам вводити
+# your_first_name = input('Ваше ім`я: ')
+# your_last_name = input('Ваше прізвище: ')
+
+#   виводимо прізвищем та ім'ям з привітанням з заглавної букви
+# print(f'Вітаю, {your_last_name.title()} {your_first_name.title()}')
+
+#   Ця програма зчитує три числа і виводить їхню суму:
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a + b + c)
+
+# num = 3.0678
+# remainder = num % 1 
+# print(remainder)
+# print(f'0.{int(remainder * 1000)}')
+
